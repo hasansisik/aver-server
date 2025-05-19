c883c2c28ef782c55cba5923f5f74adbe74dc99d
Hasan Şişik <sisik.hsn@gmail.com>
2025-05-20 00:51:48 +0300

    aver

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://aver-nextjs-tau.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
